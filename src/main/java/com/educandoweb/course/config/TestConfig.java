3842f641ad10d177bddf2768e5973d8ca81c9083
Kelvin Burti <kelvinburti@gmail.com>
2026-09-18 17:34:01 -0300

    Category

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -1,5 +1,6 @@
 package com.educandoweb.course.config;
 
+import com.educandoweb.course.repositories.CategoryRepository;
 import java.time.Instant;
 import java.util.Arrays;
 
@@ -8,6 +9,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.educandoweb.course.entities.Category;
 import com.educandoweb.course.entities.Order;
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.entities.enums.OrderStatus;
@@ -18,13 +20,26 @@ import com.educandoweb.course.repositories.UserRepository;
 @Profile("test")
 public class TestConfig implements CommandLineRunner {
 
+	private final CategoryRepository categoryRepository;
 	@Autowired
 	private UserRepository userRepository;
 	@Autowired
 	private OrderRepository orderRepository;
 
+
+
+	TestConfig(CategoryRepository categoryRepository) {
+		this.categoryRepository = categoryRepository;
+	}
+	
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+				
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
